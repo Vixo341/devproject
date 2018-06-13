3d69e3065fff42c979286e25881b014216cbcab8
vixo341 <malpa341@gmail.com>
2018-06-13 13:02:40 +0200

    final app improve

--- server.js
+++ server.js
@@ -1,47 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
 // path to files
-const posts = require('./routes/api/posts');
-const profile = require('./routes/api/profile');
-const users = require('./routes/api/users');
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+const users = require("./routes/api/users");
 
 const app = express();
 
 //body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 //db config
-const db = require('./config/keys').mongoURI;
-
+const db = require("./config/keys").mongoURI;
 
 //connect
-mongoose.connect(db)
-.then(() => console.log('db connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(db)
+  .then(() => console.log("db connected"))
+  .catch(err => console.log(err));
 
 // Passport initialize
 app.use(passport.initialize());
 
 // Passport config JWT
-require('./config/passport')(passport);
-
-
-
+require("./config/passport")(passport);
 
 //use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+// server static assets if in production
+if (process.env.NODE_ENV === "productions") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.port || 5000;
 
-app.listen(port, () => console.log('server running on port ${port}'));
-
+app.listen(port, () => console.log("server running on port ${port}"));
