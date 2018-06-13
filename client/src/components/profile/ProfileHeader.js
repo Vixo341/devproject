b2da7389774f51e8797992e5d39af30cb9775156
vixo341 <malpa341@gmail.com>
2018-06-13 12:41:16 +0200

    final application

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -21,7 +21,7 @@ class ProfileHeader extends Component {
             <div className="text-center">
               <h1 className="display-4 text-center">{profile.user.name}</h1>
               <p className="lead text-center">
-                {profile.status}
+                {profile.status}{' '}
                 {isEmpty(profile.company) ? null : (
                   <span>at {profile.company}</span>
                 )}
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://admin:admin@ds219100.mlab.com:19100/devconnectorproject',
-    secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
