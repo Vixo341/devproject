e2204be5bfd00e337f9655e2eceab28c03715d3f
vixo341 <malpa341@gmail.com>
2018-06-19 11:47:58 +0200

    deploy app

--- routes/api/users.js
+++ routes/api/users.js
@@ -64,54 +64,53 @@ User.findOne({email: req.body.email})
 });
 
 
-
-//@route GET api/users/login
-//@desc returning JWT token
+// @route   GET api/users/login
+// @desc    Login User
 router.post('/login', (req, res) => {
-    const {errors, isValid} = validateLoginInput(req.body);
-
-    if(!isValid){
-        return res.status(400).json(errors);
+    const { errors, isValid } = validateLoginInput(req.body);
+  
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
+  
     const email = req.body.email;
     const password = req.body.password;
-
-
-    //find user by email
-User.findOne({email})
-.then(user => {
-    //check for user
-    if(!user){
-        errors.email = 'USER NOT FOUND!';
+  
+    // Find user by email
+    User.findOne({ email }).then(user => {
+      // Check for user
+      if (!user) {
+        errors.email = 'User not found';
         return res.status(404).json(errors);
-    }
-
-    //check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-        if(isMatch){
-        // User Matched
-        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
-
-        // Sign Token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 3600 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: 'Bearer ' + token
-            });
-          }
-        );
-         } else{
-             errors.password = 'Password incorrect';
-            return res.status(400).json({errors});
-          }
-       });
+      }
+  
+      // Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User Matched
+          const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+  
+          // Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            }
+          );
+        } else {
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
+        }
+      });
     });
-});
-
+  });
+  
 
 
 // @route   GET api/users/current
